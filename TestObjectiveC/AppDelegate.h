ce9e73c80f131fdf9f0106bc761f29eb3a0dcc83
Ken S <kksiu@wisc.edu>
2013-09-21 01:08:14 -0500

    Added project files

---
+++ TestObjectiveC/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  TestObjectiveC
+//
+//  Created by Kenneth Siu on 9/21/13.
+//  Copyright (c) 2013 Kenneth Siu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
